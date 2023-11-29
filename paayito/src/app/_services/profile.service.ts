23e37dc6015a2b10314c536d6b2fbb15b5705124
Omar <0voynich@gmail.com>
2023-11-29 16:31:44 -0600

    add info profile

---
+++ paayito/src/app/_services/profile.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Global } from './global';
+
+@Injectable()
+export class ProfileService{
+	public url:string;
+
+	constructor(
+		private _http: HttpClient
+	){
+		this.url = Global.url;
+	}
+
+	getCartera(id: number): Observable<any>{
+		let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+		return this._http.get(this.url+'cartera-asesor/'+id, {headers: headers});
+	}
+
+}
--- paayito/src/app/pages/profile/profile.component.ts
+++ paayito/src/app/pages/profile/profile.component.ts
@@ -1,16 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../_services/profile.service';
 
+declare let $: any;
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: []
+  styleUrls: [],
+  providers: [ProfileService]
 })
 export class ProfileComponent implements OnInit {
   currentUser: any;
+  public asesor!: any;
 
-  constructor() { }
+  constructor(private _profileservice: ProfileService) { }
 
   ngOnInit(): void {
+    this.asesor = JSON.parse(localStorage.getItem('userData')!);
+    this.getCartera(this.asesor.id);
+  }
 
+  getCartera(id:any) {
+    const formatter = new Intl.NumberFormat('es-MX', {
+      style: 'currency',
+      currency: 'MXN',
+      minimumFractionDigits: 2
+    })
+    this._profileservice.getCartera(id).subscribe(
+      response => {
+        if (response != 'No existen') {
+          $('.n-prestamos').html(response[0].no_prestamos);
+          $('.total-cartera').html(formatter.format(response[0].saldo_cartera));
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
   }
 }
